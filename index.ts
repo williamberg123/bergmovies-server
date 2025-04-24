0e4bff11bf3965f53538184ce44e98171ea8adad
williamberg123 <williamberg567@gmail.com>
2025-04-24 11:48:59 -0300

    updating 2

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-import { App } from './app';
+import { App } from './src/app';
 
 new App().server.listen(5555, async () => {
 	console.log('running: http://localhost:5555/');
